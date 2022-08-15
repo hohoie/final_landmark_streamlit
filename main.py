93bf072075a7f646db772cfbb1e4d00b85cb13d2
hohoie <105341763+hohoie@users.noreply.github.com>
2022-08-15 15:47:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
+st.set_page_config(
+    page_title="Vegetable Image Dataset Midproject",
+    page_icon="🥗",
+    layout="wide",
+)
+st.sidebar.markdown("# Vegetable Image Dataset ")
+
+st.markdown('# Mid Project')
+st.markdown("## 15가지의 채소들🥗")
+st.markdown("콩, 여주, 박, 브리잘, 브로콜리, 양배추, 고추, 당근, 콜리플라워, 오이, 파파야, 감자, 호박, 무, 토마토")
+st.info(
+    '#### 데이터 출처: [Vegetable Image Dataset link](https://www.kaggle.com/datasets/misrakahmed/vegetable-image-dataset)')

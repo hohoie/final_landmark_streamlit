adff0605ba9f6040d12ab5dc87a6dea8da49d29f
hohoie <105341763+hohoie@users.noreply.github.com>
2022-08-15 15:56:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.set_page_config(
 st.sidebar.markdown("# Suoul_Landmark Dataset ")
 
 st.markdown('# Final Project')
-st.markdown("## 랜드마크~건설! (서울편)🏢")
+st.markdown("### 랜드마크~건설! (서울편)🏢")
 st.markdown("관광 산업과 AI 기반 시각 분야 기술 접목(이미지 인식) 을 통해 국내 관광 및 인공지능 산업의 글로벌 경쟁력 강화")
 st.info(
-    '## 데이터 출처: [Vegetable Image Dataset link](https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=realm&dataSetSn=56)')
+    '#### 데이터 출처: [Vegetable Image Dataset link](https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=realm&dataSetSn=56)')

8fa4f05e89619da0ffaff3f28cf968cb4e97e1bd
hohoie <105341763+hohoie@users.noreply.github.com>
2022-08-18 20:59:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ st.markdown('# Final Project')
 st.markdown("### 랜드마크 건설! 🏢")
 st.markdown("**초등학생**을 대상으로 한 랜드마크 이미지 분류")
 st.info(
-    '### 출처: [Landmark Images Dataset link](https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=realm&dataSetSn=56)')
+    '### 원본 출처: [Landmark Images Dataset link](https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=realm&dataSetSn=56)')
+
+
+
+
+
+st.markdown("![Alt Text](https://media0.giphy.com/media/ITRemFlr5tS39AzQUL/giphy.gif?cid=ecf05e47sy7qzem905gcf49g4eyrkoa82atlqlc89vu8m81i&rid=giphy.gif&ct=gf)")

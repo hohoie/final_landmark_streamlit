29e534808df484d3eb845f92dafb77ac2c398526
hohoie <105341763+hohoie@users.noreply.github.com>
2022-08-18 21:01:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.sidebar.markdown("# Suoul_Landmark Dataset ")
 
 st.markdown('# Final Project')
 st.markdown("### 랜드마크 건설! 🏢")
-st.markdown("**초등학생**을 대상으로 한 랜드마크 **딥러닝**이미지 분류")
+st.markdown("**초등학생**을 대상으로 한 랜드마크 **딥러닝** 이미지 분류")
 st.info(
     '### 원본 출처: [Landmark Images Dataset link](https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=realm&dataSetSn=56)')
 
